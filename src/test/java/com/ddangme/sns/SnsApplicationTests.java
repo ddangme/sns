11691949a6415d86238e771a01bc68c1992e24c7
ddangme <ssilver121@naver.com>
2024-02-22 17:18:54 +0900

    init: initialize project

---
+++ src/test/java/com/ddangme/sns/SnsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ddangme.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
